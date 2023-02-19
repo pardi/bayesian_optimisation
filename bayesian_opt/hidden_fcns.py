b1c6d5e5b0d02558967da409452b4afd6fec72a3
tommasopardi <tommasopardi@automata.tech>
2023-02-19 18:09:56 +0000

    feat{hidden_fcn}: updated the hidden_fcns.py file

---
+++ bayesian_opt/hidden_fcns.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+# TODO:Add decorator
+def single_var_obj_fcn(x, noise_value=0.1):
+    if noise_value > 0:
+        noise = 2 * np.random.rand(1) * noise_value - noise_value / 2
+    else:
+        noise = 0
+
+    y = x**2 * np.sin(5 * np.pi * x) ** 6
+
+    return y + noise
+
