103ed7d9d169350340a19d61fd29b3fe9c151ac7
tommasopardi <tommasopardi@automata.tech>
2023-02-19 18:25:41 +0000

    chore{flake8}: linted the file

--- bayesian_opt/hidden_fcns.py
+++ bayesian_opt/hidden_fcns.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 # TODO:Add decorator
-def single_var_obj_fcn(x, noise_value=0.1):
+def single_var_obj_fcn(x: np.array, noise_value: float = 0.1) -> np.array:
     if noise_value > 0:
         noise = 2 * np.random.rand(1) * noise_value - noise_value / 2
     else:
@@ -11,4 +11,3 @@ def single_var_obj_fcn(x, noise_value=0.1):
     y = x**2 * np.sin(5 * np.pi * x) ** 6
 
     return y + noise
-
