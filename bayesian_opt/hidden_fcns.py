48edca89696ab5521edff5d3e8a55374ed90adef
Pardi Tommaso <tommaso.pardi@gmail.com>
2023-02-22 11:21:38 +0000

    feat{hidden_fcns}: added an extra hidden function
    We added a second function to increase the testing of the bayesian optimisation.

--- bayesian_opt/hidden_fcns.py
+++ bayesian_opt/hidden_fcns.py
@@ -2,12 +2,15 @@ import numpy as np
 
 
 # TODO:Add decorator
-def single_var_obj_fcn(x: np.array, noise_value: float = 0.1) -> np.array:
+def single_var_obj_fcn(x: np.array, noise_value: float = 0.1, option: int = 0) -> np.array:
     if noise_value > 0:
         noise = 2 * np.random.rand(1) * noise_value - noise_value / 2
     else:
         noise = 0
 
-    y = x**2 * np.sin(5 * np.pi * x) ** 6
+    if option == 0:
+        y = x**2 * np.sin(5 * np.pi * x) ** 6
+    else:
+        y = x**3 + 2*x**5 + np.cos(5 * np.pi * x)
 
     return y + noise
